b3182fcfb4d7a1b1a24fa2b570fb2da2179c98ea
Edward Paget <ed.paget@gmail.com>
2014-02-27 09:52:25 -0600

    Change license to APL

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'clojure_web'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures clojure_web'
+maintainer       'Edward Paget'
+maintainer_email 'ed@zooniverse.org'
+license          'Apache Public License'
+description      'Configures server for an uberjarred application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
